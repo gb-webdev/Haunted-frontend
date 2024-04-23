a2f7b73ab98962b9aebb824c66d15190996dd5ee
Graham <blundell.gp@gmail.com>
2024-04-22 23:23:16 -0700

    added itemIndex page and edited app etc

--- haunted/src/pages/ItemIndex.js
+++ haunted/src/pages/ItemIndex.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 
-const ItemIndex = () => {
+const ItemIndex = ({ items }) => {
     return (
         <div className='index-container'>
-
+            <div className='item-card'>
+                {items.map((item) => (
+                    <div className='card-content' key={(item.id)}>
+                        <h5 className='card-title'>{item.title}</h5>
+                        <NavLink to={`/items/${item.id}`}>
+                        <button>View Item</button>
+                        </NavLink>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
